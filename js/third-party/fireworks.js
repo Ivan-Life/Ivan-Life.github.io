abbbe57ee0e5ffd1456ba37ac665b64b7c824fbe
Ivan-Life <ivan_jiang0106@163.com>
2019-10-07 22:13:58 +0800

    Site updated: 2019-10-07 22:13:58

--- js/third-party/fireworks.js
+++ js/third-party/fireworks.js
@@ -42,8 +42,8 @@ function updateCoords (e) {
 }
 
 function setParticuleDirection (e) {
-  var angle = anime.random(0, 360) * Math.PI / 180
-  var value = anime.random(50, 180)
+  var angle = anime.random(0, 250) * Math.PI / 120
+  var value = anime.random(30, 80)
   var radius = [-1, 1][anime.random(0, 1)] * value
   return {
     x: e.x + radius * Math.cos(angle),
@@ -108,20 +108,20 @@ function animateParticules (x, y) {
       return p.endPos.y
     },
     radius: 0.1,
-    duration: anime.random(1200, 1800),
+    duration: anime.random(800, 1200),
     easing: 'easeOutExpo',
     update: renderParticule
   })
     .add({
       targets: circle,
-      radius: anime.random(80, 160),
+      radius: anime.random(50, 100),
       lineWidth: 0,
       alpha: {
         value: 0,
         easing: 'linear',
-        duration: anime.random(600, 800)
+        duration: anime.random(400, 600)
       },
-      duration: anime.random(1200, 1800),
+      duration: anime.random(800, 1200),
       easing: 'easeOutExpo',
       update: renderParticule,
       offset: 0
